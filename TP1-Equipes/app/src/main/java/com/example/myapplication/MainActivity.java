ad032bef05ffdf47a7c2284c0191fd74d6bef6ae
kingnomad3 <e.bjoinvil@etu.cvm.qc.ca>
2023-09-01 12:56:44 -0400

    commentaire

--- TP1-Equipes/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TP1-Equipes/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //creation des variables
     Button atlantaButton;
     Button chicagoButton;
     Button washingtonButton;
@@ -21,17 +22,18 @@ public class MainActivity extends AppCompatActivity {
     TextView nomInput;
     ImageView teamImage;
 
-//    better to use Hashmap dans mon cas
+    //better to use Hashmap dans mon cas?
     HashMap<String, EquipeBasket> team;
-    //    List<EquipeBasket> dataList;
-    String[] teamNames = {"Atlanta", "Chicago", "Washington"};
-//    int currentIndex = 0;
+    //List<EquipeBasket> dataList;
+    //String[] teamNames = {"Atlanta", "Chicago", "Washington"};
+    //int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialisation de vlaeur
         atlantaButton = findViewById(R.id.atlantaButton);
         chicagoButton = findViewById(R.id.chicagoButton);
         washingtonButton = findViewById(R.id.washingtonButton);
@@ -39,36 +41,42 @@ public class MainActivity extends AppCompatActivity {
         nomInput = findViewById(R.id.nomEntrainer);
         teamImage = findViewById(R.id.defaultImg);
 
+        //creation/initialisation de mon ecouteur
         Ecouteur ec = new Ecouteur();
 
+        //creation de mon Hashmap
         team = new HashMap<>();
 
+        //le premier string est la cle pour chaque equipe
+        //creation de mon objet team depuis la class EquipeBasket
         team.put("Atlanta", new EquipeBasket(R.drawable.atlanta, "Gateway Center Arena", "Tanisha Wright"));
         team.put("Chicago", new EquipeBasket(R.drawable.chicago, "Wintrust Arena", "Emre Vatansever"));
         team.put("Washington", new EquipeBasket(R.drawable.washington, "Entertainment & Sports Arena", "Eric Thibault"));
 
+        //Set mes tags avec mes clee
+        //https://developer.android.com/reference/android/view/View#getTag()
+        //http://www.java2s.com/example/java-api/android/widget/textview/gettag-0-0.html
         atlantaButton.setTag("Atlanta");
         chicagoButton.setTag("Chicago");
         washingtonButton.setTag("Washington");
 
+        //associsation de mes bouttons avec mon Ecouteur
         atlantaButton.setOnClickListener(ec);
         chicagoButton.setOnClickListener(ec);
         washingtonButton.setOnClickListener(ec);
     }
 
     private class Ecouteur implements View.OnClickListener {
-
         @Override
-
-        public void onClick(View v) {
-//            get tag me permet de prendre l'objet Team que j'ai creer
-//            https://developer.android.com/reference/android/view/View#getTag()
-//            http://www.java2s.com/example/java-api/android/widget/textview/gettag-0-0.html
-            String teamName = (String) v.getTag();
+        public void onClick(View source) {
+            //get tag me permet de prendre l'objet Team que j'ai creer
+            String teamName = (String) source.getTag();
+            //appel de ma fonction qui va change l'affichage
             updateView(teamName);
         }
     }
 
+    //fonction qui me permet d'update mes valeur qui proviennent de mon hashStable
     private void updateView(String teamName) {
         EquipeBasket equipe = team.get(teamName);
         arenaView.setText(equipe.getArenaInput());
